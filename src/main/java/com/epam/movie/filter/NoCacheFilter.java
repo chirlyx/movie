5fec528b62a661cb7551f7cb66e7129f7926b7a9
chirlyx <chirlyx238@gmail.com>
2022-02-09 00:19:03 +0300

    add no-cache filter

---
+++ src/main/java/com/epam/movie/filter/NoCacheFilter.java
@@ -0,0 +1,20 @@
+package com.epam.movie.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/controller")
+public class NoCacheFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+
+        chain.doFilter(req, res);
+    }
+}
